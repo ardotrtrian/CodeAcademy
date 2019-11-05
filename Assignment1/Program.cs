21db5f91807f4f0e5d7f8d101a54d2470b07b2a2
Artavazd Trtrian <ardotrtrian@gmail.com>
2019-11-05 15:44:58 +0400

    First Assignment

--- Assignment1/Program.cs
+++ Assignment1/Program.cs
@@ -10,6 +10,31 @@ namespace Assignment1
     {
         static void Main(string[] args)
         {
+            //Problem1
+            //Write a function that copies given array of double elements into an array of inte elements:
+            double[] doubleArray = new double[] { -2.5, 5.63, 8.95, 15.23, 96.55, 78.11, 36.56, 4.44, 81.12, 66.50, -25.33 };
+
+            Array IntArray = Copy(doubleArray);
+
+            foreach (var element in IntArray)
+            {
+                Console.Write($"{element} ");
+            }
         }
+        public static Array Copy(double[] doubleArray)
+        {
+            int size = doubleArray.Length;
+
+            int[] intArray = new int[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                intArray[i] = Convert.ToInt32(doubleArray[i]);
+            }
+
+            return intArray;
+        }
+
     }
 }
+
