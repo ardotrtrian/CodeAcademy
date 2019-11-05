f318eff1dd1962daefcbff013414bff99410854e
Artavazd Trtrian <ardotrtrian@gmail.com>
2019-11-05 16:57:55 +0400

    problem2 solved

--- Assignment1/Program.cs
+++ Assignment1/Program.cs
@@ -10,18 +10,30 @@ namespace Assignment1
     {
         static void Main(string[] args)
         {
-            //Problem1
+            //Problem 1
             //Write a function that copies given array of double elements into an array of inte elements:
             double[] doubleArray = new double[] { -2.5, 5.63, 8.95, 15.23, 96.55, 78.11, 36.56, 4.44, 81.12, 66.50, -25.33 };
-
             Array IntArray = Copy(doubleArray);
-
             foreach (var element in IntArray)
             {
                 Console.Write($"{element} ");
             }
+            Console.WriteLine();
+
+            //problem 2
+            //Write a function to delete an element at desired position from an array. 
+            char[] Array2 = new char[] { 'a','b','g','t','e','u','i','z' };
+            Console.WriteLine("Enter the index of element you want to delete");
+            int elementToDelete =int.Parse(Console.ReadLine());
+            char[] resultArray = RemoveAt(Array2, elementToDelete);
+            foreach (var element in resultArray)
+            {
+                Console.Write($"{element} ");
+            }
+            Console.WriteLine();
+
         }
-        public static Array Copy(double[] doubleArray)
+        public static int[] Copy(double[] doubleArray)
         {
             int size = doubleArray.Length;
 
@@ -35,6 +47,42 @@ namespace Assignment1
             return intArray;
         }
 
+        public static T[] RemoveAt<T>(T[] source, int index) //works for all types of arrays
+        {
+            //My approach:
+            //Arrays are immutable in nature so I have to create a new array that is one element shorter 
+            //and copy the old items to the new array, excluding the element I want to delete.
+
+            if (source == null) //array is empty
+            {
+                throw new ArgumentNullException("source is null");
+            }
+
+            if (index >= source.Length || index<0) //index is out of array length range
+            {
+                throw new ArgumentOutOfRangeException($"index {index} is outside of bounds of source array");
+            }
+
+            int n = source.Length - 1;
+            T[] newArray = new T[n];
+            int newPointer = 0;  //new array pointer on index
+            int sourcePointer = 0;  //source array pointer on index
+
+            foreach (var element in source)
+            {
+                if (sourcePointer != index) 
+                {
+                    newArray[newPointer] = source[sourcePointer];
+                    newPointer++;
+                    sourcePointer++;
+                }
+                else
+                {
+                    sourcePointer++;
+                }
+            }
+            return newArray;
+        }
     }
 }
 
