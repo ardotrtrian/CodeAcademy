e8beebf5f53d4ba1a204b8aadd362a931effd0bb
Artavazd Trtrian <ardotrtrian@gmail.com>
2019-11-15 16:07:11 +0400

    Assignment 2.2-1 and 2 are done

---
+++ Assignement2.2/PrimeNumbers.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignement2._2
+{
+    class PrimeNumbers : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            var p = new PrimeNumbersEnumerator();
+            return p;
+        }
+    }
+
+    class PrimeNumbersEnumerator : IEnumerator
+    {
+        private int currentPrimeNumber = 1;
+        public object Current
+        {
+            get
+            {
+                return currentPrimeNumber;
+            }
+        }
+        public bool MoveNext()
+        {
+            bool primefound = true;
+
+            while (primefound)
+            {
+                currentPrimeNumber++;
+
+                if (currentPrimeNumber == 2)
+                {
+                    return true;
+                }
+                int i;
+
+                for (i = 2; i < currentPrimeNumber / 2 + 1; i++)
+                {
+                    if (currentPrimeNumber % i == 0)
+                    {
+                        currentPrimeNumber++;
+                        i = 2;
+                    }
+                }
+
+                if(i== currentPrimeNumber / 2 + 1)
+                {
+                    return true;
+                }
+            }
+
+            return primefound;
+        }
+        public void Reset()
+        {
+            currentPrimeNumber = 1;
+        }
+    }
+}
---
+++ Assignement2.2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignement2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create an Enumertar function Power() that returns all powers of number from 1 to its exponent
+
+            //int number = 3;
+            //int exponent = 6;
+            //foreach (var item in Power(number,exponent))
+            //{
+            //    Console.Write($"{item} ");
+            //}
+
+            //---------------------------------------------------------------------------------------------
+
+            //Create a class primeNumbers that enumerates all prime numbers.
+
+            PrimeNumbers primeNumbers = new PrimeNumbers();
+
+            IEnumerator enumerator = primeNumbers.GetEnumerator();
+
+            Console.WriteLine($"Prime numbers until 100 are:");
+            
+            while(enumerator.MoveNext())
+            {
+                if ((int)enumerator.Current > 100)
+                {
+                    break;
+                }
+
+                Console.Write($"{enumerator.Current} ");
+            }
+
+            //-------------------------------------------------------------------------------------------
+
+            //Implement a class BinaryTree and an enumerator doing a prefic traversal.
+
+            //TODO
+
+        }
+        static IEnumerable Power(int number, int exponent)
+        {
+            int index = 0;
+            int PreviousResult = 1;
+            while(exponent > index++)
+            {
+                PreviousResult = number * PreviousResult;
+                yield return PreviousResult;
+            }
+        }
+    }
+}
