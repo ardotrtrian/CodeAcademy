7836fc501505cd678263d9695a56a302157f036b
Artavazd Trtrian <ardotrtrian@gmail.com>
2019-11-22 13:19:13 +0400

    Assignment 4 started

---
+++ Assignment4/Airport.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4
+{
+    class Airport
+    {
+        public string Name { get; set; }
+        public string CountryCode { get; set; }
+        public string Size { get; set; }
+    }
+}
---
+++ Assignment4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4
+{
+    class Program
+    {
+        /* 1.
+         *  Multimap is a generalization of a map or associative array data type in which
+            more than one value may be associate with and returned for a given key[Wiki].
+            Create a generic multimap – a data structure which holds multiple values at a key.
+            This means we can add multiple values for a single key in a dictionary.
+            The implemented type should implement IDictionary interface (generic one
+            defined in System.Collections.Generic namespace), i.e. all methods defined in the
+            interface should be implemented.
+            
+            2.
+            The sizes of airports are categorized into these 5 groups: Small, Medium, Large, Mega, Super Mega.
+            Given a list of airports order them according to their size.
+         */
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
