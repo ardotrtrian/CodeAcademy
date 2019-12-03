6f30e8d48f91184c55012db4ddb55da2729f2366
Artavazd Trtrian <ardotrtrian@gmail.com>
2019-12-03 14:57:23 +0400

    added comments

--- Assignment6/Die.cs
+++ Assignment6/Die.cs
@@ -20,11 +20,12 @@ namespace Assignment6
         {
             Random rand = new Random();
 
-            int previousRoll = 0;
+            int previousRoll = 0;  
             int currentRoll;
-            int countOfTwoFours = 0;
-            int dieToss = 1;
+            int countOfTwoFours = 0;  //number of 4 tosses occured
+            int dieToss = 1;       //to keep track of die tossing process
 
+            //To save the previous 5 tosses
             List<int> FiveRollsValues = new List<int>();
 
             for (int i = 0; i < RollsCount; i++)
@@ -33,22 +34,30 @@ namespace Assignment6
 
                 currentRoll = rand.Next(1,7);
 
-                if (previousRoll == 4 && currentRoll == 4 )
+                //if the previous toss value is 4 and equal to current toss value
+                if (previousRoll == 4 && currentRoll == 4 )    
                 {
                     countOfTwoFours++;
-                    TwoFoursEventHandler.Invoke(countOfTwoFours);
+                    TwoFoursEventHandler.Invoke(countOfTwoFours);   //invoke the event 
                 }
 
-                FiveRollsValues.Add(currentRoll);
+                //add the curretn toss value to the list
+                FiveRollsValues.Add(currentRoll);   
 
+                //if the list already contains five tosses 
                 if (FiveRollsValues.Count == 5)
                 {
+                    //and sum is equal to 20 or more 
                     if (FiveRollsValues.Sum() >= 20)
                     {
-                        FiveTossesSumEventHandler.Invoke(FiveRollsValues);
+                        FiveTossesSumEventHandler.Invoke(FiveRollsValues);  //invoke the event 
                     }
+
+                    //once the event invoking process is over , remove the toss value that occured 5 tosses earlier
                     FiveRollsValues.RemoveAt(0);
                 }
+
+                //now the curretn toss value becomes the previous toss value
                 previousRoll = currentRoll;
 
                 Console.WriteLine();
