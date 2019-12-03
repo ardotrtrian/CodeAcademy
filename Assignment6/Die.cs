3d0bd9913744d788f3c6f6c582e61ab80172e152
Artavazd Trtrian <ardotrtrian@gmail.com>
2019-12-03 14:51:48 +0400

    Die Problem solved

---
+++ Assignment6/Die.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment6
+{
+    class Die
+    {
+        public delegate void TwoFoursInRowDelegate(int count);
+
+        public delegate void FiveTossesSum(List<int> Tosses);
+
+        public event TwoFoursInRowDelegate TwoFoursEventHandler;
+
+        public event FiveTossesSum FiveTossesSumEventHandler; 
+
+        public void Roll(int RollsCount)
+        {
+            Random rand = new Random();
+
+            int previousRoll = 0;
+            int currentRoll;
+            int countOfTwoFours = 0;
+            int dieToss = 1;
+
+            List<int> FiveRollsValues = new List<int>();
+
+            for (int i = 0; i < RollsCount; i++)
+            {
+                Console.Write($"Toss number {dieToss++}");
+
+                currentRoll = rand.Next(1,7);
+
+                if (previousRoll == 4 && currentRoll == 4 )
+                {
+                    countOfTwoFours++;
+                    TwoFoursEventHandler.Invoke(countOfTwoFours);
+                }
+
+                FiveRollsValues.Add(currentRoll);
+
+                if (FiveRollsValues.Count == 5)
+                {
+                    if (FiveRollsValues.Sum() >= 20)
+                    {
+                        FiveTossesSumEventHandler.Invoke(FiveRollsValues);
+                    }
+                    FiveRollsValues.RemoveAt(0);
+                }
+                previousRoll = currentRoll;
+
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
---
+++ Assignment6/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Die die = new Die();
+
+            die.TwoFoursEventHandler += TwoFoursCount;
+
+            die.FiveTossesSumEventHandler += SumIsGreaterThanTenty;
+
+            die.Roll(100);
+        }
+
+        static void TwoFoursCount(int count)
+        {
+            Console.Write($" => Two Fours in a row ({count})");
+        }
+
+        static void SumIsGreaterThanTenty(List<int> FiveToses)
+        {
+            Console.Write(" => last 5 tosses values are greater than twenty : ");
+
+            for (int i = 0; i < FiveToses.Count; i++)
+            {
+                Console.Write(FiveToses[i]);
+                Console.Write(" ");
+            }
+            Console.Write("\n");
+        }
+    }
+}
