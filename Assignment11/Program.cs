0d5ed588237d26a4c5c23851bb49bf2bb89581c4
Artavazd Trtrian <ardotrtrian@gmail.com>
2020-02-17 17:01:07 +0400

    assignment11 initial

---
+++ Assignment11/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment11
+{
+    class Program
+    {
+        /*
+         * ImageDownloader Write a program that downloads images from the specified URL, saves the images locally and rotates the images by 90 degrees.
+         * All operations should be asynchronous. 
+         * The program should allow to do these operations on a set of URLs.
+         * HttpClientclass can be usedfor downloading images from a given URL.
+         * FileStreamclasscan be used for writing bytesto a file (.jpg, or.png).
+         * Bitmap(System.Drawing), Imageclasses can be used for working withimages –reading, rotating,savingan image, etc.
+         */
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
